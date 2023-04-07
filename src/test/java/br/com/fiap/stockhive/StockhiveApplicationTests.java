7ee4c8b3599b49f2efa76ce4a20949844b14bc77
Guilherme C Do <gcd.future@gmail.com>
2023-04-07 14:23:12 -0300

    Projeto base do Spring Initializr.

---
+++ src/test/java/br/com/fiap/stockhive/StockhiveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.stockhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockhiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
